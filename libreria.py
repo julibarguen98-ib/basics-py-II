2eaa145886f6b633231ab6e369dc6a296f6f61e4
julibarguen98-ib <julibarguen98@gmail.com>
2026-09-22 21:07:26 +0200

    feat

--- libreria.py
+++ libreria.py
@@ -11,8 +11,11 @@ y que retorne un diccionario con el título y el autor del libro.
 """
 
 # Escribe tu código aquí
+def agregar_libro(titulo, autor):
+  return {"titulo":titulo, "autor":autor}
 # Prueba la función con algunos valores
-
+libro = agregar_libro("Cerdos en la sala", "Frank Hammon")
+print(libro)
 
 """
 --- Ejercicio 2: Función para Listar Libros ---
@@ -21,8 +24,20 @@ que retorne una lista con los títulos de los libros.
 """
 
 # Escribe tu código aquí
-# Prueba la función con algunos valores
-
+def listar_libros(libros):
+  titulos = []
+  for libro in libros:
+    titulos.append(libro["titulo"])
+  return titulos
+  # Prueba la función con algunos valores
+libros = [
+  {"titulo": "Hansell & Gretell", "autor": "Hermanos Grimm"},
+  {"titulo": "El gigante egoista", "autor": "Oscar Wilde"},
+  {"titulo": "Hermes y el hacha", "autor": "Esopo"}
+  ]
+  
+lista = listar_libros(libros)
+print(lista)
 
 """
 --- Ejercicio 3: Función para Buscar Libros ---
@@ -31,9 +46,20 @@ que retorne el diccionario del libro que coincida con el título, o `None` si no
 """
 
 # Escribe tu código aquí
-
+def buscar_libro (libros, titulo):
+  for libro in libros:
+    if libro["titulo"] == titulo:
+      return libro
+  return None
 # Prueba la función con algunos valores
+libros = [
+  {"titulo": "Hansell & Gretell", "autor": "Hermanos Grimm"},
+  {"titulo": "El gigante egoista", "autor": "Oscar Wilde"},
+  {"titulo": "Hermes y el hacha", "autor": "Esopo"}
+  ]
 
+print(buscar_libro(libros,"El gigante egoista"))
+print(buscar_libro(libros, "La cenicienta"))
 """
 --- Ejercicio 4: Manejo de Errores ---
 Crea una función llamada `quitar_libro` que acepte una lista de diccionarios `libros` y un `titulo` y 
@@ -41,9 +67,26 @@ que intente quitar el libro con el título especificado. Si no se encuentra el l
 """
 
 # Escribe tu código aquí
+def quitar_libro(libros, titulo):
+  try:
+    for libro in libros:
+      if libro["titulo"] == titulo:
+        libros.remove(libro)
+        return libros
 
+    raise ValueError("Libro no encontrado")
+  except ValueError as error:
+    print(error)
+    return libros
 # Prueba la función con algunos valores
+libros = [
+  {"titulo": "Hansell & Gretell", "autor": "Hermanos Grimm"},
+  {"titulo": "El gigante egoista", "autor": "Oscar Wilde"},
+  {"titulo": "Hermes y el hacha", "autor": "Esopo"}
+  ]
 
+print(quitar_libro(libros,"El gigante egoista"))
+print(quitar_libro(libros, "La cenicienta"))
 
 """
 --- Ejercicio 5: Función que Retorna un Diccionario ---
@@ -52,9 +95,23 @@ que retorne un diccionario con la cantidad de libros por autor.
 """
 
 # Escribe tu código aquí
-
+def crear_inventario(libros):
+  inventario = {}
+  for libro in libros:
+    autor = libro["autor"]
+    if autor in inventario:
+      inventario[autor] += 1
+    else :
+      inventario[autor] = 1
+  return inventario
 # Prueba la función con algunos valores
+libros = [
+  {"titulo": "Hansell & Gretell", "autor": "Hermanos Grimm"},
+  {"titulo": "Blancanieves", "autor": "Hermanos Grimm"},
+  {"titulo": "Hermes y el hacha", "autor": "Esopo"}
+  ]
 
+print(crear_inventario(libros))
 
 """
 --- Ejercicio 6: Función que Retorna una Lista ---
@@ -63,8 +120,20 @@ que retorne una lista con los títulos de los libros escritos por el autor espec
 """
 
 # Escribe tu código aquí
-
+def libros_por_autor(libros, autor):
+  titulos = []
+  for libro in libros:
+    if libro["autor"] == autor:
+      titulos.append(libro["titulo"])
+  return titulos
 # Prueba la función con algunos valores
+libros = [
+  {"titulo": "Hansell & Gretell", "autor": "Hermanos Grimm"},
+  {"titulo": "Blancanieves", "autor": "Hermanos Grimm"},
+  {"titulo": "Hermes y el hacha", "autor": "Esopo"}
+  ]
+
+print(libros_por_autor(libros, "Hermanos Grimm"))
 
 """
 --- Ejercicio 7: Función que Retorna un Booleano ---
@@ -73,6 +142,16 @@ que retorne `True` si el libro existe en la lista, y `False` en caso contrario.
 """
 
 # Escribe tu código aquí
-
+def existe_libro(libros, titulo):
+  for libro in libros:
+    if libro["titulo"] == titulo:
+      return True
+  return False
 # Prueba la función con algunos valores
+libros = [
+  {"titulo": "Hansell & Gretell", "autor": "Hermanos Grimm"},
+  {"titulo": "Hermes y el hacha", "autor": "Esopo"}
+  ]
 
+print(existe_libro(libros, "Hansell & Gretell"))
+print(existe_libro(libros, "Aladino"))
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # Añade una colección de libros
-
+from libreria import (
+    agregar_libro,
+    libros_por_autor,
+    existe_libro
+)
+libros = [
+  agregar_libro("Cerdos en la sala", "Frank Hammon"),
+  agregar_libro("Hansell & Gretell", "Hermanos Grimm"),
+  agregar_libro("Hermes y el hacha", "Esopo"),
+  agregar_libro("Blancanieves", "Hermanos Grimm"),
+  agregar_libro("El gigante egoista", "Oscar Wilde")
+]
 # Muestra la colección de libros creada
-
+print("Colección de libros:")
+print(libros)
 # Busca un libro por el autor
-
+print("\nLibros de Hermanos Grimm:")
+print(libros_por_autor(libros, "Hermanos Grimm"))
 # Verifica si un libro está disponible
+print("\n¿Existe Blancanieves?")
+print(existe_libro(libros,))
